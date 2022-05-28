0057266c7d79abc1658b20103d779b5d2920ba3d
kiwi-ui <yusnianggara@gmail.com>
2022-05-28 22:00:36 +0700

    bismillah

--- index.js
+++ index.js
@@ -3,25 +3,65 @@
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
 // const name = ["Monica","Angga","Beni","Wendy","Xora","Yana","Zainal"];
 // const gender = ["Female","Male"]
+// const favoriteResataurant = {
+//     firstUser : new Set([])
+// }
+
 const firstUser = {
     "name" : "Monica",
     "gender" : "Female" ,
     "age" : "17",
     "email" :"Monica@dingdong.com",
-    "favoriteColor":new Set('Yellow','Pink', 'White', 'Purple'),
+    "favoriteColor":new Set(['Yellow','Pink', 'White', 'Purple']),
     "isHavePet" :"Yes",
-    "education" : [['name: SD 01', 'city: Jakarta', 'graduate: 2016'],['name: SMP 02', 'city: Jakarta', 'graduate: 2019'], ['name: SMA 03', 'city: Tangerang']],
-    "favoriteResataurant": new Set('Bento','Sushi','Pancake','Eggy','Tempura','Bento','Eggy','Padang','Tteok','Sushi','Sushi')
+    "education" : [
+        {
+            name: 'SD 01',
+            city: 'Jakarta',
+            graduate: '2016'
+        },
+        {
+            name: 'SMP 02',
+            city: 'Jakarta',
+            graduate: '2019'
+        },
+        {
+            name: 'SMA 03',
+            city: 'Tangerang'
+        }
+    ],
+    "favoriteResataurant": new Set(['Bento','Sushi','Pancake','Eggy','Tempura','Bento','Eggy','Padang','Tteok','Sushi','Sushi'])
 };
 const secondUser = {
     name: 'Wendy',
     gender: 'Male',
     age: 23,
     email: 'wendy@dingdong.com',
-    favoriteColor: new Set('Blue','Black', 'Grey'),
+    favoriteColor: new Set(['Blue','Black', 'Grey']),
     isHavePet: 'No',
-    education: [['name: SD 02','city: Jakarta','graduate: 2010'],['name: SMP 03','city: Bogor','graduate: 2013'], ['name: SMA 01', 'city: Surabaya', 'graduate: 2016'], [['name: Univesitas Maju', 'city: Tanggerang']]],
-    favoriteRestaurant: new Set('Tempura', 'Bento', 'Sushi', 'Pancake', 'Padang', 'Katsu', 'Geprek', 'Pancake', 'Eggy')
+    education: 
+    [
+        {
+            name: 'SD 02',
+            city: 'Jakarta',
+            graduate: '2010'
+        },
+        {
+            name: 'SMP 03',
+            city: 'Bogor',
+            graduate: '2013'
+        },
+        {
+            name: 'SMA 01',
+            city: 'Surabaya',
+            graduate: '2016'
+        },
+        {
+            name: 'Universitas Maju',
+            city: 'Tangerang'
+        }
+    ],
+    favoriteRestaurant: new Set(['Tempura', 'Bento', 'Sushi', 'Pancake', 'Padang', 'Katsu', 'Geprek', 'Pancake', 'Eggy'])
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
