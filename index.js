f0ac6cae1583817bdaf0e6c0a0960151a3ea3d65
kiwi-ui <yusnianggara@gmail.com>
2022-05-28 21:27:39 +0700

    Bismillah test pertama lolos

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+// const name = ["Monica","Angga","Beni","Wendy","Xora","Yana","Zainal"];
+// const gender = ["Female","Male"]
+const firstUser = {
+    "name" : "Monica",
+    "gender" : "Female" ,
+    "age" : "17",
+    "email" :"Monica@dingdong.com",
+    "favoriteColor":["kl"],
+    "isHavePet" :"Yes",
+    "education" : "name: SD 01, city: Jakarta, graduate: 2016",
+    "favoriteResataurant": "Bento, Sushi, Pancake, Eggy, Tempura, Bento, Eggy, Padang, Tteok, Sushi, Sushi"
+};
+const secondUser = {
+    "name" : "Wendy",
+    "gender" : "Male" ,
+    "age" : "23",
+    "email" :"wendy@dingdong.com",
+    "favoriteColor":"Blue, Black, Grey",
+    "isHavePet" :"No",
+    "education" : [],
+    "favoriteResataurant": []
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [firstUser,secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
